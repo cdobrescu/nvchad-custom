9e31361f598b92600a1c26888b9060f770dd99b2
CDO <cdo@cdo.cdo>
2024-11-30 16:07:11 +0200

    update to nvChad v3

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -1,24 +1,47 @@
--- This file  needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
+-- This file needs to have same structure as nvconfig.lua 
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :( 
 
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = {
+M.base46 = {
 	theme = "catppuccin",
+
+	-- hl_override = {
+	-- 	Comment = { italic = true },
+	-- 	["@comment"] = { italic = true },
+	-- },
+}
+
+M.ui = {
   cmp = {
-    icons = true,
+    icons_left = false, -- only for non-atom styles!
     lspkind_text = true,
     style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+    format_colors = {
+      tailwind = false, -- will work for css lsp too
+      icon = "󱓻",
+    },
   },
 
   telescope = { style = "borderless" }, -- borderless / bordered
-------------------------------- nvchad_ui modules -----------------------------
+
   statusline = {
+    enabled = true,
+    theme = "default", -- default/vscode/vscode_colored/minimal
     -- default/round/block/arrow separators work only for default statusline theme
     -- round and block will work for minimal theme only
     separator_style = "block",
     order = nil,
+    modules = nil,
+  },
+
+  -- lazyload it when there are 1+ buffers
+  tabufline = {
+    enabled = true,
+    lazyload = true,
+    order = { "treeOffset", "buffers", "tabs", "btns" },
     modules = nil,
   },
 }
